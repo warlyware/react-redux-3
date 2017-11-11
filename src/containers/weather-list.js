cfb565b4ba2e4a268443a15ccf407f969fa9dcc0
warlyware <danward@gmail.com>
2017-11-10 19:20:01 -0500

    Use sparklines to display temparature data

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
 
-  renderWeather(response) {
+  renderWeather(weather) {
+    const name = weather.city.name;
+    const temperatures = weather.list.map(weather => weather.main.temp);
+
     return (
-      <tr key={response.city.name}>
-        <td>{response.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temperatures}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
